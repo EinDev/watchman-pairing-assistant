6a12c83ba8d5613980693ef181126c40e923056a
Tayou <git@tayou.org>
2026-06-28 14:15:10 +0200

    change repo back to mine

--- nix/package.nix
+++ nix/package.nix
@@ -36,8 +36,8 @@ python3Packages.buildPythonApplication {
   ];
 
   meta = {
-    homepage = "https://github.com/EinDev/watchman-pairing-assistant";
-    downloadPage = "https://github.com/EinDev/watchman-pairing-assistant/releases";
+    homepage = "https://github.com/TayouVR/watchman-pairing-assistant";
+    downloadPage = "https://github.com/TayouVR/watchman-pairing-assistant/releases";
     description = "GUI for pairing SteamVR Tracking devices using lighthouse_console";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [
